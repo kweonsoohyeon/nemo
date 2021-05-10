4422d5eb48798ce7e92aaf59f044a3d0fc3d4ae5
kweonsoohyeon <kweonsh9503@naver.com>
2021-05-10 22:08:02 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {Button,Progress} from 'semantic.ui.react'
+
+function App() {
+  return (
+    <p> 안녕하세요</p>
+
+  );
+}
+
+export default App;
